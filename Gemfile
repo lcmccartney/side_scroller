d98a34334ac5e11e02c9e747e52b5fa870a638c6
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-04-08 11:48:04 -0600

    inital classes made

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'chipmunk'
+gem 'gosu'
+gem 'pry'
---
+++ escape.rb
@@ -0,0 +1,33 @@
+require 'gosu'
+require 'chipmunk'
+require 'pry'
+require_relative 'tumbler'
+
+class Escape < Gosu::Window
+  DAMPING = 0.90
+  GRAVITY = 400.0
+  attr_reader :space
+
+  def initialize
+    super(800, 800)
+    self.caption = 'Escape'
+    @game_over = false
+    @space = CP::Space.new
+    @background = Gosu::Image.new('images/background.png', tileable: true)
+    @space.damping = DAMPING
+    @space.gravity = CP::Vec2.new(0.0, GRAVITY)
+  end
+
+  def update
+    unless @game_over
+      10.times do
+        @space.step(1.0/600)
+      end
+    end
+  end
+
+end
+
+window = Escape.new
+window.show
+Tumbler.new(window, 0.0, 0.0)
